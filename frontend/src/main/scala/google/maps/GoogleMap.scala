b8c2ea1544df076666b95ee0a7f42cc7595ca30f
Vladimir Pavkin <vpavkin@gmail.com>
2016-06-06 09:28:34 +0300

    Add google maps library wrapper

---
+++ frontend/src/main/scala/google/maps/GoogleMap.scala
@@ -0,0 +1,9 @@
+package google.maps
+
+import org.scalajs.dom.raw.Element
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSName
+
+@js.native
+@JSName("google.maps.Map")
+class GoogleMap(node: Element, options: js.Dynamic = js.Dynamic.literal()) extends js.Object
---
+++ frontend/src/main/scala/google/maps/GoogleMapMarker.scala
@@ -0,0 +1,10 @@
+package google.maps
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSName
+
+@js.native
+@JSName("google.maps.Marker")
+class GoogleMapMarker(options: js.Dynamic) extends js.Object {
+  def addListener(event: String, callback: js.Function1[js.Any, Unit]): Unit = js.native
+}
